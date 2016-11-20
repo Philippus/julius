43dde0fe29bf517405c455a265dfb63d6e665464
Philippus Baalman <philippus@gmail.com>
2016-11-21 00:02:04 +0100

    added the possibility to add a RomanDigit to a RomanNumeral and vice versa, so we can do things like: - X + RomanNumeral(List(M)) + I

--- src/main/scala/JuliusImplicits.scala
+++ src/main/scala/JuliusImplicits.scala
@@ -1,5 +1,8 @@
 object JuliusImplicits {
   implicit class RomanDigitToInt(r: RomanDigit) {
+    def +(that: RomanNumeral): RomanNumeral = {
+      RomanNumeral(List(r)) + that
+    }
     def toInt: Int = {
       r match {
         case I => 1
@@ -13,6 +16,9 @@ object JuliusImplicits {
     }
   }
   implicit class RomanNumeralToInt(n: RomanNumeral) {
+    def +(that: RomanDigit): RomanNumeral = {
+      n + RomanNumeral(List(that))
+    }
     def toInt: Int = n match {
       case RomanNumeral.Nulla => 0
       case RomanNumeral.RomanDigits(l) => l.map(_.toInt).sum
