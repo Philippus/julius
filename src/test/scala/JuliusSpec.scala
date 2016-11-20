773de24a13529a247f25ab4f794b7519de6d985a
Philippus Baalman <philippus@gmail.com>
2016-11-20 21:20:20 +0100

    added generator for RomanNumeral

--- src/test/scala/JuliusSpec.scala
+++ src/test/scala/JuliusSpec.scala
@@ -1,13 +1,25 @@
 import org.scalacheck.Gen._
-import org.scalacheck.{Arbitrary, Properties}
+import org.scalacheck.{Arbitrary, Gen, Properties}
 import org.scalacheck.Prop.forAll
 
-object JuliusSpec extends Properties("RomanDigit") {
+object JuliusSpec extends Properties("Julius") {
   implicit val arbitraryRomanDigit: Arbitrary[RomanDigit] = Arbitrary(
     oneOf(List(M, D, C, L, X, V, I))
   )
 
-  property("generator only generates roman digits") = forAll {
+  def romanNumerals: Gen[RomanNumeral] = Gen.oneOf(genNulla, genRomanDigits)
+
+  def genNulla: Gen[RomanNumeral] = RomanNumeral()
+
+  def genRomanDigits: Gen[RomanNumeral] = for {
+    x <- nonEmptyListOf(oneOf(List(M, D, C, L, X, V, I)))
+  } yield RomanNumeral(x)
+
+  property("RomanDigit.generator only generates roman digits") = forAll {
     r: RomanDigit => r == I || r == V || r == X || r == L || r == C || r == D || r == M
+  }
+
+  property("RomanNumeral.generator only generates roman numerals") = forAll(romanNumerals) {
+    n: RomanNumeral => n == RomanNumeral.Nulla || n.isInstanceOf[RomanNumeral.RomanDigits]
   }
 }
