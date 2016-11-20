3f53a5c329ddcafacd8715e8bfbb84f61ff3c53b
Philippus Baalman <philippus@gmail.com>
2016-11-20 12:17:10 +0100

    added test for the generator

--- src/test/scala/JuliusSpec.scala
+++ src/test/scala/JuliusSpec.scala
@@ -1,8 +1,13 @@
 import org.scalacheck.Gen._
 import org.scalacheck.{Arbitrary, Properties}
+import org.scalacheck.Prop.forAll
 
 object JuliusSpec extends Properties("RomanDigit") {
   implicit val arbitraryRomanDigit: Arbitrary[RomanDigit] = Arbitrary(
     oneOf(List(M, D, C, L, X, V, I))
   )
+
+  property("generator only generates roman digits") = forAll {
+    r: RomanDigit => r == I || r == V || r == X || r == L || r == C || r == D || r == M
+  }
 }
