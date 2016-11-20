16c1c990be85e283b77913c291e9e08191f02425
Philippus Baalman <philippus@gmail.com>
2016-11-20 21:39:23 +0100

    use pattern matching, removing an isInstanceOf

--- src/test/scala/JuliusSpec.scala
+++ src/test/scala/JuliusSpec.scala
@@ -18,11 +18,22 @@ object JuliusSpec extends Properties("Julius") {
   implicit val arbitraryRomanNumeral: Arbitrary[RomanNumeral] = Arbitrary(genRomanNumeral)
 
   property("RomanDigit.generator only generates roman digits") = forAll {
-    r: RomanDigit => r == I || r == V || r == X || r == L || r == C || r == D || r == M
+    r: RomanDigit => r match {
+      case I => true
+      case V => true
+      case X => true
+      case L => true
+      case C => true
+      case D => true
+      case M => true
+    }
   }
 
   property("RomanNumeral.generator only generates roman numerals") = forAll {
-    n: RomanNumeral => n == RomanNumeral.Nulla || n.isInstanceOf[RomanNumeral.RomanDigits]
+    n: RomanNumeral => n match {
+      case RomanNumeral.Nulla => true
+      case RomanNumeral.RomanDigits(_) => true
+    }
   }
 
   property("RomanNumeral should be Nulla or consist of one or more digits") = forAll {
