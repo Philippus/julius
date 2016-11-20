3dadc2aa40a423e1e0e64d258504674eaa25dcd8
Philippus Baalman <philippus@gmail.com>
2016-11-20 12:04:06 +0100

    sealed trait for the different roman digits

---
+++ src/main/scala/RomanDigit.scala
@@ -0,0 +1,8 @@
+sealed trait RomanDigit
+case object I extends RomanDigit
+case object V extends RomanDigit
+case object X extends RomanDigit
+case object L extends RomanDigit
+case object C extends RomanDigit
+case object D extends RomanDigit
+case object M extends RomanDigit
