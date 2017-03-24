d14f1c90fdfeb2abd4c2421976e5c8d90782e56f
Philippus Baalman <philippus@gmail.com>
2017-03-24 19:13:24 +0100

    Move generators to a separate file

--- src/test/scala/JuliusSpec.scala
+++ src/test/scala/JuliusSpec.scala
@@ -1,38 +1,12 @@
 import scala.annotation.tailrec
-import scala.collection.immutable.Stream
 
-import org.scalacheck.Gen._
 import org.scalacheck._
 import org.scalacheck.Prop.forAll
-import org.scalacheck.Shrink.shrink
 
 import JuliusImplicits._
 
 object JuliusSpec extends Properties("Julius") {
-  def genRomanDigit: Gen[RomanDigit] = oneOf(List(M, D, C, L, X, V, I))
-
-  implicit val arbitraryRomanDigit: Arbitrary[RomanDigit] = Arbitrary(genRomanDigit)
-
-  def genLimitedRomanNumeral: Gen[RomanNumeral] = genRomanNumeral retryUntil (_.toInt < 1000)
-
-  def genRomanNumeral: Gen[RomanNumeral] = Gen.oneOf(genNulla, genRomanDigits)
-
-  def genNulla: Gen[RomanNumeral] = RomanNumeral()
-
-  def genRomanDigits: Gen[RomanNumeral] = for {
-    x <- nonEmptyListOf(genRomanDigit)
-  } yield RomanNumeral(x)
-
-  def genStringOfRomanDigits: Gen[String] = for {
-    x <- listOf(genRomanDigit)
-  } yield x.mkString
-
-  implicit val arbitraryRomanNumeral: Arbitrary[RomanNumeral] = Arbitrary(genRomanNumeral)
-
-  implicit val shrinkRomanNumeral: Shrink[RomanNumeral] = Shrink {
-    case RomanNumeral.Nulla => Stream.empty
-    case RomanNumeral.RomanDigits(l) => shrink(l).map(RomanNumeral.apply)
-  }
+  import Generators._
 
   property("RomanDigit.generator only generates roman digits") = forAll {
     r: RomanDigit => r match {
